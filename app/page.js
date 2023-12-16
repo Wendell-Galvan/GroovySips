a5cb49bd85775684580ac8812d9d2312e9736d90
Wendell Galvan <you@example.com>
2023-12-15 21:14:23 -0800

    created pricing package cards

--- app/page.js
+++ app/page.js
@@ -8,36 +8,36 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div className="h-full mx-auto">
-      <div className="p-4">
+      <div className="mt-4">
         <h1
           className={`${notoSerif.className} flex justify-center font-bold text-2xl md:text-4xl`}
         >
           So Happy You're Here
         </h1>
       </div>
-      <div className="flex justify-center items-center flex-col md:flex-row mt-6">
+      <div className="flex justify-center items-center flex-col lg:flex-row lg:justify-evenly">
         <Image
           src={profile}
           alt="profile"
-          height={300}
-          width={300}
+          height={500}
+          width={400}
           className="object-cover"
         />
-        <div className={`${montserrat.className} text-left max-w-md md:ml-20`}>
-          <p className="my-2">
+        <div className={`${montserrat.className} text-left max-w-md`}>
+          <p className="mb-2">
             We are GroovySips. With over a decade of service industry experience
             and 8 years experience of operating a family owned business, we are
-            so excited to be joining the event and wedding industry. Being apart
-            of other’s special events brings us joy and getting to make
+            so excited to be joining the event and wedding industry. Being a
+            part of other’s special events brings us joy and getting to make
             connections through these interactions, no matter how small, is what
             drives our business.
           </p>
-          <p className="my-2">
+          <p className="my-4">
             Whether you are planning a large wedding, small holiday party, or
             anything in between. We are thrilled to help make your event
             extraordinary by providing our friendly and professional services.
           </p>
-          <p className="">
+          <p className="mb-2">
             We understand that life looks differently for everyone. Please,
             reach out to us if there is not an option or package that fits your
             budget and we would be more than happy to chat. Afterall, it’s YOUR
--- app/pricing/page.js
+++ app/pricing/page.js
@@ -1,11 +1,12 @@
 import { Noto_Serif, Playfair } from "next/font/google";
+import Table from "../components/Table";
 
 const notoSerif = Noto_Serif({ subsets: ["latin"] });
 const playFair = Playfair({ subsets: ["latin"] });
 
 const Pricing = () => {
   return (
-    <div className="h-screen mx-auto">
+    <div className="h-full mx-auto">
       <div className="p-4">
         <h1
           className={`${notoSerif.className} flex justify-center font-bold text-2xl md:text-4xl`}
@@ -17,20 +18,9 @@ const Pricing = () => {
         <h2 className={`${playFair.className} flex justify-center text-xl`}>
           PACKAGES
         </h2>
-        <p className="flex justify-center m-4">
-          asdas asd asadasdasd asdasda asdasd aasdasdas asdasd asdasd sadas asd
-          asdasd asda dasdas dasdas das fasfsadf asfa fasd afs asf
-        </p>
+        <Table />
       </div>
-      <div className="container mx-auto mt-10">
-        <h2 className={`${playFair.className} flex justify-center text-xl`}>
-          Included
-        </h2>
-        <p className="flex justify-center m-4">
-          asdas asd asadasdasd asdasda asdasd aasdasdas asdasd asdasd sadas asd
-          asdasd asda dasdas dasdas das fasfsadf asfa fasd afs asf
-        </p>
-      </div>
+      <div></div>
     </div>
   );
 };
